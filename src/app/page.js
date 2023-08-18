ac0f4e453ed773cca0d5c17e65444e148547ddca
Senalov <senalovaziz12@gmail.com>
2023-08-18 18:39:52 +0600

    add sigUp page and logIn page

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 import styles from './page.module.css'
 import Insta from '../components/insta'
+import UserLogin from '../components/auth/user'
 export default function Home() {
   return (
     <main className={styles.main}>
+      <UserLogin />
         <Insta />
     </main>
   )
